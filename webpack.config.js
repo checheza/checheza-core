0265c402d263f65453158985e38618e8de9e506f
Andréas Forsbom <andreasforsbom@gmail.com>
2020-05-22 00:53:27 +0200

    Updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,17 +3,26 @@ const path = require('path');
 module.exports = {
   mode: 'production',
   entry: {
-    core: [ './src/core.js' ],
+    core: [ '@babel/polyfill/noConflict', 'jszip' ,'./src/core.js' ],
   }, 
   target: 'node',
   output: {
-    library: 'Core',
+    library: 'checheza_core',
     libraryTarget: 'umd',
-    filename: 'dist.js',
+    globalObject: 'this',
+    filename: 'core.js',
     path: path.resolve(__dirname, './')
   },
   module: {
     rules: [ 
+      {
+        test: /\.css$/i,
+        use: [ { loader: 'style-loader' }, { loader: 'css-loader', options: { url: true, modules: true } }],
+      },
+      {
+        test: /\.(jpe?g|png|ttf|eot|svg|wav|ogg|mp3|otf|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+        use: 'base64-inline-loader?limit=1000&name=[name].[ext]'
+      },
       {
         test: /\.js$/,
         exclude: /(node_modules)/,
